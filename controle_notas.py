616334a99661a31262546d0237592bea2bf81700
lucastoupitzen <80696823+lucastoupitzen@users.noreply.github.com>
2021-10-04 09:38:22 -0300

    Update controle_notas.py

--- controle_notas.py
+++ controle_notas.py
@@ -3,7 +3,7 @@
 import pandas as pd  # importa a bibloteca pandas
 
 
-def analisa_nota(nota):
+def analisa_nota(nota): # analisa o resultado das notas dos alunos e retorna se passou ou não
     resultado = 0
     if nota < 5.0:
         resultado = "Não"
@@ -14,23 +14,23 @@ def analisa_nota(nota):
     return resultado
 
 
-def retorna_nomes():
+def retorna_nomes(): # input do nome do aluno ou da chave de saída
     nome = str(input("Nome do aluno ou digite Sair para sair "))
     return nome
 
 
-def retorna_notas():
+def retorna_notas(): # input da nota do aluno
     nota = float(input("Nota do aluno "))
     return nota
 
 
-def corrige_nome(nome):
+def corrige_nome(nome): # corrige espaços na frente do nome, viabilizando a chave de saída
     while nome[0] == " ":
         nome = nome[1:]
     return nome
 
 
-def controle(lista_nome, lista_notas, lista_passou):
+def controle(lista_nome, lista_notas, lista_passou): # retorna o DataFrame da biblioteca pandas
     controle = pd.DataFrame(
         {"Nome": lista_nome,
             "Nota": lista_notas,
@@ -45,10 +45,10 @@ def main():
     lista_notas = []
     lista_passou = []
     nome = 0  # controle de quando deve encerrar
-    while nome != "Sair":
+    while nome != "Sair": # controla a chave de saída do código
         nome = retorna_nomes()
         nome = corrige_nome(nome)
-        if nome != "Sair":
+        if nome != "Sair": # se for igual a Sair -> sai do laço
             lista_nome.append(nome)
             nota = retorna_notas()
             lista_notas.append(nota)
